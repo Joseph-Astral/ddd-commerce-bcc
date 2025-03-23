6ba39136f489fd2063a928d05b6d3fea9449223a
José Victor Tranquilino <josevictortranquilino746@gmail.com>
2025-03-23 20:11:21 -0300

    Add files via upload

---
+++ DDDCommerceBCC.Domain/Pedido.cs
@@ -0,0 +1,24 @@
+﻿namespace DDDCommerceBCC.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; private set; }
+        public string Cliente { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+        public decimal ValorTotal { get; private set; }
+
+        public Pedido(int id, string cliente, DateTime dataCriacao, decimal valorTotal)
+        {
+            Id = id;
+            Cliente = cliente;
+            DataCriacao = dataCriacao;
+            ValorTotal = valorTotal;
+        }
+
+        // Exemplo de método de domínio
+        public void AtualizarValorTotal(decimal novoValor)
+        {
+            ValorTotal = novoValor;
+        }
+    }
+}
